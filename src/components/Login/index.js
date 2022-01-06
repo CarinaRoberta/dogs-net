3c7e70509ba7d1bdd86e798774d7b12e7e00d09a
CarinaRoberta <silva.carina@live.com>
2022-01-06 20:44:06 -0300

    creation login page without style

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import LoginCreate from "./components/LoginForm";
+import LoginPasswordLost from "./components/LoginForm";
+import LoginPasswordReset from "./components/LoginForm";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;

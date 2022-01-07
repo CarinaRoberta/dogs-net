29bb89018cf13d1f7208571d2973dda67743b8c8
CarinaRoberta <silva.carina@live.com>
2022-01-07 20:07:41 -0300

    add validade input username and password

---
+++ src/hooks/useForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+    message: "Preencha um e-mail válido",
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState(null);
+
+  function onChange({ target }) {
+    if (error) {
+      validate(target.value);
+    }
+    setValue(target.value);
+  }
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError("Preencha um valor");
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+    error,
+  };
+};
+
+export default useForm;

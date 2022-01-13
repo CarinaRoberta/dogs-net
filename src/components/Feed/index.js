84502360948f0e2a90ec9db39f547517abb8fb14
CarinaRoberta <silva.carina@live.com>
2022-01-12 21:26:02 -0300

    add photo comments when user logged

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import FeedModals from "./components/FeedModals";
 import FeedPhotos from "./components/FeedPhotos";
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = useState(null);
   return (
     <div>
-      <FeedModals />
-      <FeedPhotos />
+      {modalPhoto && (
+        <FeedModals photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
 };
